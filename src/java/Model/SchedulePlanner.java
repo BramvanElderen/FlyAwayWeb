e557014b11f0e042e4df0c1060978a214953d98b
Bram van Elderen <bram_bram10@hotmail.com>
2012-10-11 22:32:36 +0200

    CommentedCode verwijderd in scheduleplanner

--- src/java/Model/SchedulePlanner.java
+++ src/java/Model/SchedulePlanner.java
@@ -13,25 +13,13 @@ import java.util.ArrayList;
  */
 public class SchedulePlanner {
 
-    private User u;
 
     public SchedulePlanner() {
     }
 
     public ArrayList<Flight> getPlanning(String id) {
 	User user = Controller.Instance().getUserByUsername(id);
-	//ArrayList<Flight> flights = Controller.Instance().getFlights();
 	ArrayList<Flight> flights = Controller.Instance().getScheduledFlights(user);
-
-//        u = Controller.Instance().getUserByUsername(id);
-//        ArrayList<Flight> tempFlights = Controller.Instance().getFlights();
-//        ArrayList<Flight> flights = null;
-//        
-//        for (int i = 0; i < tempFlights.size(); i++) {
-//            if(tempFlights.get(i).getPilot().getName().equals(u.getUsername())){
-//                flights.add(tempFlights.get(i));
-//            }
-//        }
 
 	return flights;
     }
